8c3fe370998391d9ec9a92ea71594b342c7126c5
MacaroniMutton <mananskher@gmail.com>
2024-11-26 13:10:32 +0530

    Modified api.js to have VITE_API_URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://patient-health-monitoring-system-02b7.onrender.com';
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 const api = axios.create({
   baseURL: API_URL,
